5268cce69e9227aa0982874426ef792c1fb9f91a
Javier Quintana <javierquintanamartin84@gmail.com>
2023-07-11 21:52:19 +0200

    Install Laravel Mix and change version bootstrap-sass to avoid error on compiling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 
         <!-- Styles -->
         <style>
@@ -91,5 +93,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
