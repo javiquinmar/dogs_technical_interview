1a91c36407c650a067192e46afc097aea399fbf9
Javier Quintana <javierquintanamartin84@gmail.com>
2023-07-13 00:26:52 +0200

    Add dogs relations

--- app/Dog.php
+++ app/Dog.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dog extends Model
 {
-    //
+    protected $fillable = ['name', 'breed_id', 'color_id', 'size_id', 'age', 'weight'];
+
+    public function breed(){
+        return $this->belongsTo(Breed::class);
+    }
+
+    public function color(){
+        return $this->belongsTo(Color::class);
+    }
+
+    public function size(){
+        return $this->belongsTo(Size::class);
+    }
 }
