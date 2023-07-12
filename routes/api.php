68b4390ed7a7273cd121a811026391062ba7568b
Javier Quintana <javierquintanamartin84@gmail.com>
2023-07-13 00:11:33 +0200

    Add api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('dogs', 'DogController@index')->name('dogs.index');
+Route::post('dogs', 'DogController@store')->name('dogs.store');
+Route::put('dogs/{id}', 'DogController@update')->name('dogs.update');
+Route::get('breeds', 'BreedController@index')->name('breeds.index');
+Route::get('colors', 'ColorController@index')->name('colors.index');
+Route::get('sizes', 'SizeController@index')->name('sizes.index');
