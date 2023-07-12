3938f80551f15a04548c2b2b88f2f63e408081c2
Javier Quintana <javierquintanamartin84@gmail.com>
2023-07-12 09:28:36 +0200

    Any route load page-manager view

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+//Route::get('/', function () {
+//    return view('page-manager');
+//});
+
+Route::get('{any}', function () {
     return view('page-manager');
-});
+})->where('any', '.*');
